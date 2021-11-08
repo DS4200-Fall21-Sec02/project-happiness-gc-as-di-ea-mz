1741960b0f070c3b3d4bb72eedcd1993f789cee4
EstherAnem <86205146+EstherAnem@users.noreply.github.com>
2021-11-08 17:26:02 -0500

    Console log prints data

--- js/visualization.js
+++ js/visualization.js
@@ -1,7 +1,6 @@
 // Immediately Invoked Function Expression to limit access to our 
 // variables and prevent 
-((() => {
+d3.csv("data/2020.csv").then((data) => {
 
-  console.log('Hello, world!');
-
-})());
+  console.log(data);
+});
