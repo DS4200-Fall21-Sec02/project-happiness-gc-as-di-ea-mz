8e51a86774dc18cc049b5afae50bc24d0e6e3550
rexzhang66 <72946512+rexzhang66@users.noreply.github.com>
2021-11-15 17:18:07 -0500

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -160,13 +160,16 @@ d3.csv("data/2020v2.csv").then(function(data) {
       .attr("dy", ".71em")
       .text(function(d) { return d.x; });
 
+
+
+
   cell.call(brushFx);
 
   svg2.selectAll("mydots")
   .data(traits)
   .enter()
   .append("rect")
-    .attr("x", 900)
+    .attr("x", 700)
     .attr("y", function(d,i){ return 50 + i*(15 + 5)}) // 100 is where the first dot appears. 25 is the distance between dots
     .attr("width", 15)
     .attr("height", 15)
@@ -177,7 +180,7 @@ d3.csv("data/2020v2.csv").then(function(data) {
   .data(traits)
   .enter()
   .append("text")
-    .attr("x", 900 + 15*1.2)
+    .attr("x", 700 + 15*1.2)
     .attr("y", function(d,i){ return 50 + i*(15+5) + (15/2)}) // 100 is where the first dot appears. 25 is the distance between dots
     .style("fill", function(d){ return color(d)})
     .text(function(d){ return d})
